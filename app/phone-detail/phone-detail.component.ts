1d832bfb35ebb5f2f7f0cf2be0ec3064dac01213
John Brecht <john.brecht@gmail.com>
2018-04-27 17:47:43 -0700

    convert controllers to classes, added ng2 dependencies

--- app/phone-detail/phone-detail.component.ts
+++ app/phone-detail/phone-detail.component.ts
@@ -1,20 +1,26 @@
 'use strict';
-
 // Register `phoneDetail` component, along with its associated controller and template
+
+class PhoneDetailController {
+  phone: any;
+  mainImageUrl: string;
+
+  static $inject = ['$routeParams', 'Phone']
+  
+  constructor($routeParams: angular.route.IRouteParamsService, Phone: any) {
+    this.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+      this.setImage(phone.images[0]);
+    })
+  }
+
+  setImage = function setImage(imageUrl: string) {
+    this.mainImageUrl = imageUrl;
+  };
+}
+
 angular.
   module('phoneDetail').
   component('phoneDetail', {
     templateUrl: 'phone-detail/phone-detail.template.html',
-    controller: ['$routeParams', 'Phone',
-      function PhoneDetailController($routeParams, Phone) {
-        var self = this;
-        self.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-          self.setImage(phone.images[0]);
-        });
-
-        self.setImage = function setImage(imageUrl) {
-          self.mainImageUrl = imageUrl;
-        };
-      }
-    ]
+    controller: PhoneDetailController
   });
